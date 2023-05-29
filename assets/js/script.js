2c31551a97094abeeb8487b7a69360d3c2674a89
Camilo Martinez <camarti@live.cl>
2023-05-29 03:10:42 -0400

    responsividad actualizada

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const flipButton = document.createElement('button');
     const scaleButton = document.createElement('button');
     const skewButton = document.createElement('button');
+    const errorMessage = document.getElementById('error-message');
+    const mainContainer = document.getElementById('main_container');
+
+
+
+
+    // Comprobar el tamaño de la pantalla cuando se carga la página y cuando se cambia su tamaño
+    checkWindowSize();
+    window.addEventListener('resize', handleResize);
 
 
 
@@ -121,6 +130,32 @@ document.addEventListener('DOMContentLoaded', () => {
     function skewReverse() {
         digimonImage.style.transform = 'skew(0, 0)';
     }
+
+    function checkWindowSize() {
+        if (window.innerWidth < 770) {
+            errorMessage.style.display = 'block'; // Mostrar el mensaje de error
+            mainContainer.style.display = 'none'; //Ocultar ventana principal
+        } else {
+            errorMessage.style.display = 'none'; // Ocultar el mensaje de error
+            mainContainer.style.display = 'flexbox'; //Mostrar ventana principal
+        }
+    }
+
+    function handleResize() {
+        if (window.innerWidth < 770) {
+            if (errorMessage.style.display !== 'block') {
+                errorMessage.style.display = 'flex'; // Mostrar el mensaje de error si no está visible
+                mainContainer.style.display = "none";
+            }
+        } else {
+            if (errorMessage.style.display !== 'none') {
+                errorMessage.style.display = 'none'; // Ocultar el mensaje de error si no está oculto
+                mainContainer.style.display = 'flex';
+            }
+        }
+    }
+
+
 
 
 });
